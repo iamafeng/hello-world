3d7d3dc946122dabe78435a00a7e8860bd3cae28
hugenfeng <hugf@z-code.cn>
2019-01-22 09:15:50 +0800

    m

--- src/hgf/com/controller/bugfix2.java
+++ src/hgf/com/controller/bugfix2.java
@@ -2,4 +2,10 @@ package hgf.com.controller;
 
 public class bugfix2 {
     //here is bug_fix_2
+    private void bugfixModifyBugfix2() {
+        //
+//        bugfix modify bugfix2
+//        6bu6
+    }
+
 }
